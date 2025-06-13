6a9d1b1dc82a6fff16bf7ff31115bd7aa6857a66
Boy Prakasa <boyprakasa@gmail.com>
2025-06-13 10:05:56 +0700

    remove routes

--- src/Providers/BsreServiceProvider.php
+++ src/Providers/BsreServiceProvider.php
@@ -19,25 +19,11 @@ class BsreServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        // Memuat route dari paket
-        $this->loadRoutesFrom(__DIR__ . '/../../routes/bsre.php');
-
-        // Memuat migration dari paket
-        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
-
         // Mengizinkan user mempublikasikan file config dan migration
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/../../config/bsre.php' => config_path('bsre.php'),
             ], 'bsre-config');
-
-            // $this->publishes([
-            //     __DIR__ . '/../../database/migrations/' => database_path('migrations'),
-            // ], 'bsre-migrations');
-
-            $this->publishes([
-                __DIR__ . '/../../routes/bsre.php' => base_path('routes/bsre.php'),
-            ], 'bsre-routes');
         }
     }
 }
